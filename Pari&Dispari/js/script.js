e73cadb4d39fcd60df9857e02d4ac4709720912a
chiaraxs <chiaracardinale92@gmail.com>
2021-12-16 18:38:35 +0100

    Pari&Dispari - da completare

--- Pari&Dispari/js/script.js
+++ Pari&Dispari/js/script.js
@@ -5,9 +5,15 @@
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
 
+// Utente inserisce PARI/DISPARI (prompt, puoi tenere proprio la stringa che inserisce l'utente, oppure 0/1 con parseInt)
+// Utente inserisce numero da 1 a 5
+// Genero numero casuale per computer, da 1 a 5
+// Sommo i due numeri
+// Controllo se la somma è pari/dispari
+// Verifico se il pari/dispari della somma coincide con la scelta che ho fatto io all'inizio
+// Se sì, ho vinto
+// Altrimenti, ho perso
+
 // 1. l'utente sceglie un numero (tra 1 e 5), pari o dispari
  let userNumber = prompt('Scegli se inserire un numero pari o dispari')
 
-// 2. creo array con numeri pari
-
-// 3 creo array con numeri dispari
